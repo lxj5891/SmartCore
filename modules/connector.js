6e2377a8ca1a913540550e11990caf9e747c24f0
LI LIN <r2space@gmail.com>
2013-05-13 11:32:34 +0800

    添加了一个泛用的mod

---
+++ modules/connector.js
@@ -0,0 +1,55 @@
+
+var Mongodb = require('mongodb')
+  , Db = Mongodb.Db
+  , Server = Mongodb.Server
+  , ObjectID = Mongodb.ObjectID
+  , conf = process.env['TEST'] ? require('config').testdb : require('config').db;
+
+/**
+ *
+ */
+var ServerOptions = {
+    auto_reconnect: false
+  , poolSize: 2
+  , socketOptions: {
+        timeout: 0
+      , noDelay: true
+      , keepAlive: 0
+      }
+  };
+
+/**
+ *
+ */
+var DbOptions = {
+    native_parser: false
+  , retryMiliSeconds: 5000
+  , strict: true
+  , w: 1
+  };
+
+/**
+ *
+ */
+function Connector(config, serveroption, dboption) {
+  this.connector = new Db(
+      config.dbname
+    , new Server(config.host, config.port, serveroption)
+    , dboption);
+
+  var self = this;
+  this.connector.open(function(err, db){
+    self.db = db;
+    self.err = err;
+  });
+}
+
+Connector.prototype.id = function(id) {
+  if (id instanceof ObjectID) {
+    return id;
+  }
+  
+  return new Object(id);
+}
+
+module.exports = new Connector(conf, ServerOptions, DbOptions);
---
+++ modules/mod_datastore.js
@@ -0,0 +1,61 @@
+/**
+ * 泛用文书存储模块
+ *
+ */
+
+var connector = require('./connector');
+
+
+/**
+ * Schema description
+ *
+ */
+
+
+exports.create = function(storeid, document, callback) {
+
+  var collection = connector.db.collection(storeid);
+  collection.insert(document, {w: 1}, function(err, result){
+    callback(err, result);
+  });
+
+};
+
+exports.update = function(storeid, condition, document, callback) {
+
+  var collection = connector.db.collection(storeid);
+
+  collection.update(condition, {$set: document}, {upsert:true, w: 1}, function(err, result){
+    callback(err, result);
+  });
+
+};
+
+exports.updateById = function(storeid, docid, document, callback) {
+
+  var condition = {_id: connector.id(docid)};
+  exports.update(storeid, condition, document, callback);
+
+}
+
+exports.find = function(storeid, condition, start_, limit_, callback) {
+
+  var collection = connector.db.collection(storeid);
+
+  collection.find(condition, {skip: start_, limit: limit_}).toArray(function(err, result){
+    callback(err, result);
+  });
+
+};
+
+exports.findById = function(storeid, docid, callback) {
+
+  var collection = connector.db.collection(storeid);
+  var condition = {_id: connector.id(docid)};
+
+  collection.findOne(condition, function(err, result){
+    callback(err, result);
+  });
+
+};
+
