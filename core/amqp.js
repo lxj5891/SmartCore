08096e47c2300276df0b10bba7b62e1afbd96d49
tomxu <cntomxu@gmail.com>
2013-10-22 21:39:49 +0800

    amqp追加切断连接操作

--- core/amqp.js
+++ core/amqp.js
@@ -7,15 +7,7 @@ var amqp = require('amqp')
 
 
 exports.smartThumb = function(message){
-  var connection = amqp.createConnection(thumbq);
-  var maxListeners = thumbq.maxListeners || 0;
-
-  connection.setMaxListeners(maxListeners);
-
-  connection.on('ready', function () {
-    
-    connection.publish(thumbq.queue, message);
-  });
+  publish(thumbq, thumbq.queue, message);
 };
 
 /**
@@ -26,29 +18,14 @@ exports.smartThumb = function(message){
  *  , _id: Collection中的ID
  */
 exports.send = function(message){
-  
-  var connection = amqp.createConnection(mq);
-  var maxListeners = mq.maxListeners || 0;
-
-  connection.setMaxListeners(maxListeners);
-
-  connection.on('ready', function () {
-    connection.publish(mq.queue, message);
-  });
+  publish(mq, mq.queue, message);
 };
 
 /**
  * 通知
  */
 exports.notice = function(message) {
-  var connection = amqp.createConnection(mq);
-  var maxListeners = mq.maxListeners || 0;
-
-  connection.setMaxListeners(maxListeners);
-
-  connection.on('ready', function() {
-    connection.publish(mq.notification_queue, message);
-  });
+  publish(mq, mq.queue, message);
 };
 
 
@@ -56,28 +33,26 @@ exports.notice = function(message) {
  * 处理图片？
  */
 exports.sendPhoto = function(message){
-  var connection = amqp.createConnection(mq_photo);
-  var maxListeners = mq_photo.maxListeners || 0;
-
-  connection.setMaxListeners(maxListeners);
-
-  connection.on("ready", function(){
-    connection.publish(mq_photo.queue, message);
-  });
+  publish(mq_photo, mq_photo.queue, message);
 };
 
 /**
  * 通过APN发送通知
  */
 exports.sendApn = function(message){
-  var connection = amqp.createConnection(mq_apn);
-  var maxListeners = mq_apn.maxListeners || 0;
+  publish(mq_apn, mq_apn.queue, message);
+};
 
-  connection.setMaxListeners(maxListeners);
+function publish(mqConfig, queueName, message) {
+  var mqConn = amqp.createConnection(mqConfig);
+  var maxListeners = mqConfig.maxListeners || 0;
+  mqConn.setMaxListeners(maxListeners);
 
-  connection.on("ready", function(){
-    connection.publish(mq_apn.queue, message, { mandatory: true }, function(){
-      connection.end();
+  var options = { mandatory: true };
+
+  mqConn.on("ready", function(){
+    mqConn.publish(queueName, message, { mandatory: true }, function(){
+      mqConn.end();
     });
   });
-};
+}
