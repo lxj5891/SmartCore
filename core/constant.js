a5ec450dfbca1e1d5d51f8ee1db0f0d067b58095
slsay <shengli1222@163.com>
2013-11-14 15:11:07 +0800

    临时保存

--- core/constant.js
+++ core/constant.js
@@ -10,4 +10,13 @@ exports.INVALID = 0;
 /**
  * Modules层用常量
  */
+// 检索件数的默认值
 exports.MOD_DEFAULT_LIMIT = 20;
+// number of connections in the connection pool, set to 5 as default.
+exports.MOD_DB_SERVER_OPTIONS = { poolSize: 2 };
+
+exports.MOD_DB_OPTIONS = { w: 1 };
+// write in truncate mode. Existing data will be Overwrite.
+exports.MOD_GRIDSTORE_MODE_WRITE = "w";
+// read only. This is the default mode.
+exports.MOD_GRIDSTORE_MODE_READ = "r";
