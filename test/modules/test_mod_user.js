fe019685172099112d6e860c53a38e5091d07ff0
nana77sun <nana77_sun@sina.com>
2013-11-21 17:29:16 +0800

    用户，组，权限 提交

--- test/modules/test_mod_user.js
+++ test/modules/test_mod_user.js
@@ -9,7 +9,7 @@
 require("../../core/test").befor();
 
 var should   = require("should")
-  , util  = require("../../core/util")
+  , util     = require("../../core/util")
   , modUser  = require("../../coverage/modules/mod_user");
 
 describe("modules/mod_user.js", function() {
