708ea21fd9ca5d93f7d618657c74dd996d560526
LI LIN <r2space@gmail.com>
2013-11-26 15:13:24 +0800

    对应新的用户结构

--- support/init_default_user.js
+++ support/init_default_user.js
@@ -1,28 +1,65 @@
-var cmd = require("./command");
+/**
+ * @file 缺省管理员用户信息登陆
+ * @author r2space@gmail.com
+ * @copyright Dreamarts Corporation. All Rights Reserved.
+ */
 
-var data = {
-  "uid" : "admin",
-  "password" : "e02fbcbf42ccce4c2fab1aff636885153b2eda49681984b7baaaf2f780482014",
-  "type" : 2,
-  "lang" : "ja",
-  "createat" : new Date(),
-  "createby" : "script",
-  "valid" : 1,
-  "authority" : {
-    "approve" : 0,
-    "notice" : 0,
-    "contents" : 0
-  },
-  "name" : {
-    "name_zh" : "admin"
-  },
-  "active" : 1,
-  "timezone" : "GMT+09:00"
+"use strict";
+
+process.env.NODE_CONFIG_DIR = __dirname + "/config";
+process.env.LOG4JS_CONFIG = __dirname + "/config/log4js.json";
+
+var program   = require("commander")
+  , cmd       = require("../lib/command");
+
+/**
+ * 显示帮助信息
+ */
+function showHelp() {
+
+  // 定义帮助信息
+  program
+    .version("0.0.1")
+    .option("-d, --db", "database name")
+    .option("-c, --collection", "user collection name");
+
+  // 定义帮助信息中的例子
+  program.on("--help", function(){
+    console.log("  Examples:");
+    console.log("");
+    console.log("    $ node support/init_default_user.js -d yukari -c users");
+    console.log("");
+  });
+
+  program.parse(process.argv);
 }
 
-cmd.insertData("yukari", "users", data, function(err, result){
-  if (err) {
-    return console.log(err);
-  }
-  console.log("insert data ok");
-});
+/**
+ * 插入用户信息
+ */
+function insertUserData() {
+  var userData = {
+      "userName": "admin"
+    , "first": "admin"
+    , "last": ""
+    , "password": "e3cfa171629240c8991f31af049b8b22a24c90ac4dc49ceb202faf7fb50bde07"
+    , "email": "admin@dreamarts.co.jp"
+    , "lang" : "ja"
+    , "timezone": "GMT+09:00"
+    , "createAt": new Date()
+    , "createBy": "script"
+    , "updateAt": new Date()
+    , "updateBy": "script"
+    };
+
+  cmd.insertData("yukari", "users", userData, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("insert data ok");
+  });
+}
+
+showHelp();
+insertUserData();
