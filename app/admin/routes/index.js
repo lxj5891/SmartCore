088227b1dc531eff517342b2ce33f662094791e4
nana77sun <nana77_sun@sina.com>
2013-11-26 18:11:31 +0800

    用户，组，权限 提交

--- app/admin/routes/index.js
+++ app/admin/routes/index.js
@@ -8,4 +8,14 @@ exports.guidingApi = function(app){
 
     res.render("admin_home", {"title": "title"});
   });
-}
+
+  app.get("/user/add", function (req, res) {
+
+    res.render("user/add", {"title": "添加用户"});
+  });
+
+  app.get("/group/add", function (req, res) {
+
+    res.render("group/add", {"title": "添加用户"});
+  });
+};
