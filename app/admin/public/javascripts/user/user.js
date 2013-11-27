2bca1885ce4abd14a279e0c91b41587663334e68
nana77sun <nana77_sun@sina.com>
2013-11-27 16:45:38 +0800

    用户，组，权限 提交

--- app/admin/public/javascripts/user/user.js
+++ app/admin/public/javascripts/user/user.js
@@ -15,4 +15,25 @@ function render() {
 
 function events() {
 
+  $("#save").click(save);
+}
+
+function save() {
+
+  var user = {
+    userName : $("#userName").val(),
+    password : $("#password").val(),
+    last     : $("#last").val(),
+    middle   : $("#middle").val(),
+    first    : $("#first").val(),
+    groups   : [],
+    email    : $("#email").val(),
+    lang     : $("#lang").val(),
+    timezone : $("#timezone").val(),
+    status   : $("#status").val()
+  };
+
+
+
+
 }
