b4948dd0e96188e0aa48e4610be8a54b18359a17
LI LIN <r2space@gmail.com>
2013-12-02 19:42:20 +0800

    添加启动脚本

--- app.js
+++ app.js
@@ -3,105 +3,43 @@
  * Module dependencies.
  */
 
-var express     = require("express")
-  , http        = require("http")
-  , ejs         = require("ejs")
-  , path        = require("path")
+"use strict";
+
+GLOBAL.smart = require("./index");
+
+var http        = smart.lang.http
+  , express     = smart.util.express
+  , middleware  = smart.framework.middleware
+  , loader      = smart.framework.loader
+  , log         = smart.framework.log
   , routes      = require("./app/admin/routes");
 
 var app = express();
 
-app.configure(function(){
-  app.set("port", process.env.PORT || 3000);
-
-  /**
-   * Middleware
-   * 生成?准favicon.ico，防止favicon.ico的404??
-   */
-  // app.use(express.favicon());
-
-  /**
-   * Middleware
-   * ??Access log和Error log
-   */
-  app.use(express.logger("dev"));
-
-  /**
-   * Middleware
-   * ??response data?gzip
-   */
-  // app.use(express.compress());
-
-  /**
-   * Middleware
-   * 包含json(), urlencoded(), multipart()三个middleware
-   */
-//  app.use(express.bodyParser({"uploadDir": confapp.tmp}));
-
-  /**
-   * Middleware
-   * 用于模?DELETE and PUT方法
-   * 可以在form里放在<input type="hidden" name="_method" value="put" />来模?
-   */
-  app.use(express.methodOverride());
-
-  /**
-   * Middleware
-   * 解析cookie
-   */
-//  app.use(express.cookieParser(confcookie.secret));
-
-  /**
-   * Middleware
-   * 提供基于cookie的session
-   */
-//  app.use(express.session({
-//      "secret": confsession.secret
-//    , "key": confsession.key
-//    , "cookie": {"maxAge": confsession.timeout * 60 * 60 * 1000}
-//    , "store": new store({"db": confdb.dbname, "host": confdb.host, "port": confdb.port})
-//    })
-//  );
-
-  /**
-   * Middleware
-   * CSRF支持。需要在?定csrftoken的前面。
-   */
-//  app.use(express.csrf());
-
-//  /**
-//   * 系?定?
-//   */
-//  app.use(middleware.authenticate); // ??
-//  app.use(middleware.csrftoken);    // 生成CsrfToken
-//  app.use(middleware.lang);         // ?定?言
-//  app.use(middleware.parseError);   // ?常?理
-//  app.use(middleware.timeout);      // ?定超?
-
-  app.use(express.static(__dirname + "/app"));
-  // 端口
-  app.set("port", process.env.PORT || 3000);
-  app.set("views", path.join(process.cwd(), "app/admin/views"));
-  app.set("view engine", "html");
-  app.engine("html", ejs.__express);
-
-});
+/**
+ * 初始化smartcore模块
+ */
+loader.initialize();
 
 /**
- * 以??模式??是有效的?定放在?里
+ * 初始化express模块
  */
-app.configure("development", function(){
-  app.use(express.errorHandler());
-});
+loader.express(app);
+
+app.use(middleware.lang);         // 设定语言
+//app.use(middleware.loadCompany);  // 加载用户的公司信息
+app.use(middleware.authenticate); // 认证
+app.use(middleware.csrftoken);    // 生成CsrfToken
+app.use(middleware.timeout);      // 设定超时
 
 /**
- * route
+ * 路由
  */
-routes.guidingApi( app );
+routes.guidingApi(app);
 
 /**
  * 启动服务
  */
 http.createServer(app).listen(app.get("port"), function(){
-  console.log("Express server listening on port " + app.get("port"));
+  log.info("Express server listening on port " + app.get("port"));
 });
---
+++ config/default.js
@@ -0,0 +1,84 @@
+module.exports = {
+
+  "db": {
+      "host": "mongo"
+    , "port": 27017
+    , "dbname": "smartcore"
+    , "prefix": "sc"
+    , "schema": {
+        "User": "Users"
+      }
+    , "pool": 5
+    }
+
+  , "testdb": {
+      "host": "mongo"
+    , "port": 27017
+    , "dbname": "developer"
+    , "pool": 5
+    }
+
+  , "mail": {
+      "service": "Gmail"
+    , "auth": {
+        "user": "smart@gmail.com"
+      , "pass": "smart"
+      }
+    }
+  
+  , "app": {
+      "port": 3000
+    , "views": "app/admin/views"
+    , "public": "/app"
+    , "cookieSecret": "smartcore"
+    , "sessionSecret": "smartcore"
+    , "sessionKey": "smartcore.sid"
+    , "sessionTimeout": 720 // 24 * 30 一个月
+    , "tmp": "/tmp"
+    , "hmackey": "smartcore"
+    , "i18n": {
+        "cache": "memory"
+      , "lang": "zh"
+      , "category": "yukari"
+      }
+    , "ignoreAuth": [
+
+        "/"
+      , "/login"
+        // 静态资源
+      , "^\/stylesheets"
+      , "^\/javascripts"
+      , "^\/vendor"
+      , "^\/images"
+      , "^\/video"
+
+        // 登陆，注册相关
+      , "^\/$"
+      , "^\/simplelogin.*"
+      , "^\/simplelogout.*"
+      , "^\/login.*"
+      , "^\/register.*"
+      ]
+    }
+
+  , "log": {
+      "fluent": {
+        "enable": "false"
+      , "tag": "node"
+      , "host": "10.2.8.228"
+      , "port": 24224
+      , "timeout": 3.0
+      }
+    }
+
+  , "mq": {
+      "host": "mq"
+    , "port": 5672
+    , "user": "guest"
+    , "password": "guest"
+    , "queue_join": "smartJoin"
+    , "queue_apn": "smartApn"
+    , "queue_thumb": "smartThumb"
+    , "maxListeners": 0
+    }
+  };
