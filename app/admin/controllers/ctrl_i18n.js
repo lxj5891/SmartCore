1602444ee8949fe61db6902c676c3e3e4dc9e329
sl_yang <shengli1222@163.com>
2013-12-19 18:34:19 +0800

    i18n

--- app/admin/controllers/ctrl_i18n.js
+++ app/admin/controllers/ctrl_i18n.js
@@ -6,8 +6,9 @@
 
 "use strict";
 
-var _           = smart.util.underscore
-  , i18n        = smart.ctrl.i18n;
+var i18n        = smart.ctrl.i18n
+  , log         = smart.framework.log
+  , _           = smart.util.underscore;
 
 
 /**
@@ -19,11 +20,17 @@ exports.getList = function(handler ,callback) {
 
 
   i18n.getList(handler, function(err, result) {
+
+    var params = handler.params
+      , uid    = params.uid;
+
+    log.debug("begin: getList keyword", uid);
+
     if (err) {
-//      log.error(err, uid);
-//      return callback(new errors.InternalServer(err));
-      return false;
+      log.error(err, uid);
+      return callback(err);
     }
+
     var i18nData = [];
 
     _.each(result, function(data) {
@@ -31,30 +38,41 @@ exports.getList = function(handler ,callback) {
       var tmpI18nData = {
           category: data.category
         , key: data.key
-        , zh: "lang"
-        , ja: "lang"
-        , en: "lang"
         };
+
+      _.each(data.lang, function(val, key) {
+        tmpI18nData[key] = val;
+      });
+
       i18nData.push(tmpI18nData);
     });
+
+    log.debug("finish: getList keyword", uid);
+
     return callback(err, i18nData);
   });
 };
 
 /**
- * 获取词条一览
+ * 更新词条
  * @param {Object} handler 上下文对象
  * @param {Function} callback 回调函数，返回翻译结果一览
  */
 exports.update = function(handler ,callback) {
 
-  console.log(handler);
+  var params = handler.params
+    , uid    = params.uid;
+
+  log.debug("begin: update keyword", uid);
+
   i18n.add(handler, function(err, result) {
+
     if (err) {
-//      log.error(err, uid);
-//      return callback(new errors.InternalServer(err));
-      return false;
+      log.error(err, uid);
+      return callback(err);
     }
+
+    log.debug("finished: update keyword", uid);
 
     return callback(err, result);
   });
